2eb66165e5139881960e1d2080ab5bd62bb338ba
Paulina Wall <paulina_wall@outlook.com>
2020-12-30 20:03:13 +0100

    render users projects from db

--- src/components/UserPages/UserProjectPage.js
+++ src/components/UserPages/UserProjectPage.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
+
+import useGetPortfolio from '../../hooks/useGetPortfolio';
 
 const UserProjectPage = () => {
+	const { portfolio } = useGetPortfolio();
+
 	return ( 
-		<h1>UserProjectPage</h1>
+		<Row>
+			{
+				(portfolio && portfolio.projects) && portfolio.projects.map((project, index) => {
+					return <Col  className="mb-3" sm={6} md={4} lg={3} key={index}>
+							<Card className="card">
+								<Card.Img variant="top" src={project.image.url} />
+								<Card.Body className="size-sm p-1.5">
+									<Card.Title>{project.title}</Card.Title>
+									<Card.Text>
+										{project.text}
+									</Card.Text>
+								</Card.Body>
+								<Card.Body className="p-1.5">
+									<Card.Link href={`http://${project.projectUrl}`}>{project.projectUrl}</Card.Link>
+								</Card.Body>
+							</Card>
+						</Col>
+				})
+			}
+		</Row>
 	 );
 }
  
