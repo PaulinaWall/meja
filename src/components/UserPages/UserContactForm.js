96db7c42ecad29029c8e44a9d0ce218aa714693e
Paulina Wall <paulina_wall@outlook.com>
2021-01-04 09:20:24 +0100

    created contact form for the userPage and a footer with links

--- src/components/UserPages/UserContactForm.js
+++ src/components/UserPages/UserContactForm.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 const UserContactForm = () => {
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('submitted');
+	}
+
 	return ( 
-		<h1>UserContactForm</h1>
+		<Form onSubmit={handleSubmit}>
+			<Form.Group controlId="exampleForm.ControlInput1">
+				<Form.Label>Email address</Form.Label>
+				<Form.Control type="email" placeholder="name@example.com" />
+			</Form.Group>
+			<Form.Group controlId="exampleForm.ControlInput1">
+				<Form.Label>First Name</Form.Label>
+				<Form.Control type="text" placeholder="Anna" />
+			</Form.Group>
+			<Form.Group controlId="exampleForm.ControlInput1">
+				<Form.Label>Last Name</Form.Label>
+				<Form.Control type="text" placeholder="Andersson" />
+			</Form.Group>
+			<Form.Group controlId="exampleForm.ControlTextarea1">
+				<Form.Label>Example textarea</Form.Label>
+				<Form.Control as="textarea" rows={6} />
+			</Form.Group>
+			<Button className="button" type="submit">
+				Submit
+			</Button>
+		</Form>
 	 );
 }
  
