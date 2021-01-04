73bea65a3264a32579904e82aecdc14852fd547e
Paulina Wall <paulina_wall@outlook.com>
2021-01-04 09:15:47 +0100

    fixes for userProjectsPage

--- src/components/UserPages/UserProjectPage.js
+++ src/components/UserPages/UserProjectPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Row, Col, Image } from 'react-bootstrap';
 
 import useGetPortfolio from '../../hooks/useGetPortfolio';
 
@@ -7,26 +7,36 @@ const UserProjectPage = () => {
 	const { portfolio } = useGetPortfolio();
 
 	return ( 
-		<Row>
-			{
-				(portfolio && portfolio.projects) && portfolio.projects.map((project, index) => {
-					return <Col  className="mb-3" sm={6} md={4} lg={3} key={index}>
-							<Card className="card">
-								<Card.Img variant="top" src={project.image.url} />
-								<Card.Body className="size-sm p-1.5">
-									<Card.Title>{project.title}</Card.Title>
-									<Card.Text>
-										{project.text}
-									</Card.Text>
-								</Card.Body>
-								<Card.Body className="p-1.5">
-									<Card.Link href={`http://${project.projectUrl}`}>{project.projectUrl}</Card.Link>
-								</Card.Body>
-							</Card>
-						</Col>
-				})
-			}
-		</Row>
+		<>
+			<Row>
+				{
+					(portfolio && portfolio.projects) && portfolio.projects.map((project, index) => {
+						return <Col  className="mb-3" sm={6} md={4} lg={3} key={index}>
+								<Card className="card">
+									{
+										project.image.url 
+										? (
+											<Card.Img variant="top" src={project.image.url} />
+										) : (
+											<Image src="holder.js/171x180" thumbnail />
+										)
+									}
+									
+									<Card.Body className="size-sm p-1.5">
+										<Card.Title>{project.title}</Card.Title>
+										<Card.Text>
+											{project.text}
+										</Card.Text>
+									</Card.Body>
+									<Card.Body className="p-1.5">
+										<Card.Link target="_blank" rel="noopener noreferrer" href={`http://${project.projectUrl}`}>{project.projectUrl}</Card.Link>
+									</Card.Body>
+								</Card>
+							</Col>
+					})
+				}
+			</Row>
+		</>
 	 );
 }
  
