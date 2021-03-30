6420d7774c3e0d6650184a01b1e132589296adca
paulina_wall <paulina.wall@eletive.com>
2021-03-30 22:35:20 +0200

    changed style for mobile view

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import Image from 'react-bootstrap/Image';
-import Writer from "../assets/images/Writer.png"
 
 const LandingPage = () => {
 	return ( 
-		<Container className="center landingPage">
-			<div className="d-flex mt-4">
-				<Image className="mr-4" width="300" src={Writer} roundedCircle />
-				<h1 className="mt-5">MEJA</h1>
-			</div>
+		<Container className="landingPage">
+			<h1 className="mt-5">MEJA</h1>
 			<p className="mt-5">Makes it easy to create a cool portfolio in a few steps. <a href="value">SAMPLE</a></p>
 		</Container>
 	 );
